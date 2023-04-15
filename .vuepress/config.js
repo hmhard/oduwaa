794e4f64bec031225da78bc666d1508c656315ef
Miniyahil Kebede <miniyahil_kebede@et.jsi.com>
2023-04-15 08:07:16 +0300

    Initial setup

---
+++ .vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'oDUWA',
+  description: 'oduwaas',
+  base:  '/oduwaa/',
+  themeConfig: {
+    sidebar: [
+      ['/', 'Introduction'],
+      '/docs/development',
+      '/docs/architecture',
+      '/docs/naming-cheatsheet',
+      // '/docs/routing',
+      // '/docs/state',
+      '/docs/linting',
+      // '/docs/editors',
+      // '/docs/production',
+      // '/docs/troubleshooting',
+    ],
+  },
+};
